91a8c4f8b36cf083de0e9a5df87e541cd7766bb3
hero <duddnd4457@naver.com>
2023-05-21 18:31:13 +0900

    index 14 done

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,6 +1,19 @@
 "use strict";
 
-console.log("Hello");
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("#loginBtn");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    console.log(req);
+}
+
 
 
 
