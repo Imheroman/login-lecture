6e11a155eebf4ff4ff942680a6d98580d8f28e74
hero <duddnd4457@naver.com>
2023-05-23 03:42:01 +0900

    index 28 done, register func done

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -5,18 +5,22 @@ const fs = require("fs").promises;
 class UserStorage {
     static #getUserInfo(data, id) {
         const users = JSON.parse(data);
-                const idx = users.id.indexOf(id);
-                const userKeys = Object.keys(users);
-                const userInfo = userKeys.reduce((newUser, info) => {
-                newUser[info] = users[info][idx];
-                return newUser;
-            }, {});
+        const idx = users.id.indexOf(id);
+        const userKeys = Object.keys(users);
+        const userInfo = userKeys.reduce((newUser, info) => {
+        newUser[info] = users[info][idx];
+        return newUser;
+        }, {});
 
-            return userInfo;
+        return userInfo;
     }
 
-    static getUsers(...fields) {
-        // const users = this.#users;
+    static #getUsers(data, isAll, fields) {
+        const users = JSON.parse(data);
+
+        if (isAll)
+            return users;
+
         const newUsers = fields.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
@@ -26,21 +30,35 @@ class UserStorage {
 
         return newUsers;
     }
-    static getUserInfo(id) {
+
+    static getUsers(isAll, ...fields) {
         return fs
             .readFile("./src/databases/users.json")
-            .then(data => {
-                return this.#getUserInfo(data, id);
+            .then((data) => {
+                return this.#getUsers(data, isAll, fields);
         })
         .catch(err => console.error);
     }
 
-    static save(userInfo) {
-        // const users = this.#users;
+    static getUserInfo(id) {
+        return fs
+            .readFile("./src/databases/users.json")
+            .then((data) => {
+                return this.#getUserInfo(data, id);
+        })
+        .catch(console.error);
+    }
+
+    static async save(userInfo) {
+        const users = await this.getUsers(true);
+        if (users.id.includes(userInfo.id))
+            throw "이미 존재하는 아이디입니다.";
+
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
-        // console.log(users);
+
+        fs.writeFile("./src/databases/users.json", JSON.stringify(users));
         return { success: true };
     }
 }
